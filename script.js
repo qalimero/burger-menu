cbd4350e8d9fc9f2dc20445cc2dbad5ea6698636
qalimero <q.serda@LGOP3661.local>
2022-02-16 23:26:20 +0100

    burger menu js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log('connecté'); 
 
+//Je selectionne et je stock les éléments necessaires
+//L'icone burger
+const icone = document.querySelector('.navbar-mobile i');
+//La div modal
+const modal = document.querySelector('.modal');
+// Je soumets l'icone à une action au clique
+icone.addEventListener('click', function(){
+    console.log('icone cliquée');
+    //Alterner les class
+    icone.classList.toggle('fa-times');
+    modal.classList.toggle('change-modal');
+});
